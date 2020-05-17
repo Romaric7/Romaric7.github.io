9850d28f1e39ff6cc3a4bfb432e66cebc269bc5b
Romaric7 <64481167+Romaric7@users.noreply.github.com>
2020-05-17 14:11:43 +0100

    mise à jour finale du dépôt

--- index_account.php
+++ index_account.php
@@ -28,20 +28,30 @@
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('images/img-01.jpg');">
 			<div class="wrap-login100 p-t-190 p-b-30">
-				<form action="login.php" method="POST" class="login100-form validate-form" autocomplete="off">
+				<form action="create_account.php" method="POST" class="login100-form validate-form" autocomplete="off">
 					<div class="login100-form-avatar">
 						<img src="images/mix.jpg" alt="AVATAR">
 					</div>
 
 					<span class="login100-form-title p-t-20 p-b-45">
 						Developped By Cletus Rom@ric7
-					</span>
+                    </span>
+                    
+                   
 
 					<div class="wrap-input100 validate-input m-b-10" data-validate = "Username is required">
 						<input class="input100" type="text" name="username" placeholder="Username">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-user"></i>
+						</span>
+                    </div>
+                    
+                    <div class="wrap-input100 validate-input m-b-10" data-validate = "email address is required">
+						<input class="input100" type="text" name="email" placeholder="Email address">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope-o"></i>
 						</span>
 					</div>
 
@@ -51,24 +61,34 @@
 						<span class="symbol-input100">
 							<i class="fa fa-lock"></i>
 						</span>
+                    </div>
+                    
+                    <div class="wrap-input100 validate-input m-b-10" data-validate = "Phone number is required">
+						<input class="input100" type="text" name="number" placeholder="Phone number">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-phone"></i>
+						</span>
 					</div>
 
 					<div class="container-login100-form-btn p-t-10">
 						<button class="login100-form-btn">
-							Login
+							Create account
 						</button>
 					</div>
 
 					<div class="text-center w-full p-t-25 p-b-100">
-						<a href="#" class="txt1">
+						<!--<a href="#" class="txt1">
 							Forgot Username / Password?
-						</a>
-					</div>
+						</a>-->
+                    </div>
+                    
+                    <br><br><br>
 
 					<div class="text-center w-full">
-						<a class="txt1" href="index_account.php">
-							Create new account
-							<i class="fa fa-long-arrow-right"></i>						
+                        <i class="fa fa-long-arrow-left"></i>
+						<a class="txt1" href="index.php">
+							Login interface						
 						</a>
 					</div>
 				</form>
